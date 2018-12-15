c0c1512bf2a151d94fe0c81bc70213e2737cc5e8
Andrew Briz <briz.andrew@gmail.com>
2018-12-15 16:52:06 -0500

    explicit nodenv

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,18 @@
 "use strict";
 
+var _path = require("path");
+
+var _path2 = _interopRequireDefault(_path);
+
 var _gapi = require("./gapi");
 
 var _gapi2 = _interopRequireDefault(_gapi);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-require('dotenv').config();
+require('dotenv').config({
+  path: _path2.default.join(__dirname, '../.env')
+});
 
 const client = new _gapi2.default();
 client.downloadImages();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import path from 'path';
 import GAPI from './gapi';
 
-require('dotenv').config();
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const client = new GAPI();
 client.downloadImages();
